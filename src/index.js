b969baf38ae612dbe83c67e62d1522f99507c56a
DriesU <91673757+DriesU@users.noreply.github.com>
2024-10-21 16:47:24 +0200

    Fix linting errors automatically

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import Boom from '@hapi/boom';
-import morgan from 'morgan'
-import persistence from './persistence/index.js'
+import express from "express";
+import Boom from "@hapi/boom";
+import morgan from "morgan";
+import persistence from "./persistence/index.js";
 
-const PORT = 3000
+const PORT = 3000;
 
 function asyncMiddleware(fn) {
   return (req, res, next) => {
@@ -14,24 +14,24 @@ function asyncMiddleware(fn) {
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.use((_, res, next) => {
-  res.set('X-Database-Used', process.env.MYSQL_URL ? 'MySQL' : 'SQLite');
+  res.set("X-Database-Used", process.env.MYSQL_URL ? "MySQL" : "SQLite");
   next();
 });
 
-app.get('/', (_, res) => {
-  res.send('It works, good job! You should try <code>/animals</code> or <code>/animals/:id</code>.');
+app.get("/", (_, res) => {
+  res.send("It works, good job! You should try <code>/animals</code> or <code>/animals/:id</code>.");
 });
 
-app.get('/animals', asyncMiddleware(async (_, res) => {
+app.get("/animals", asyncMiddleware(async (_, res) => {
   const animals = await persistence.getAnimals();
   res.json(animals);
 }));
 
-app.get('/animals/:id', asyncMiddleware(async (req, res) => {
+app.get("/animals/:id", asyncMiddleware(async (req, res) => {
   const animal = await persistence.getAnimal(Number(req.params.id));
   res.json(animal);
 }));
@@ -42,7 +42,7 @@ app.use((err, _, res, next) => {
       error: err.message,
     });
   next();
-})
+});
 
 persistence
   .initialize()
@@ -52,7 +52,7 @@ persistence
     });
   })
   .catch((err) => {
-    console.error('Database failed to connect, check the error below');
+    console.error("Database failed to connect, check the error below");
     console.error(err);
     process.exit(1);
   });
