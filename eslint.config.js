32478f63cc0c437b0682b329f20dc92b01f68779
DriesU <91673757+DriesU@users.noreply.github.com>
2024-10-21 16:37:43 +0200

    Migrate ESLint configuration to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+// eslint.config.js
+export default [
+  {
+    ignores: ["node_modules/**"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      // Add more rules as needed
+    },
+  },
+];
